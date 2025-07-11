20bdab380025a82c7e8111e2105e5d2e6b8ebcdc
Tracking <divery579@gmail.com>
2025-07-11 23:59:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Package Tracker API is running!');
+});
 
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 // Sample tracking data (can later be replaced with MongoDB)
 const trackingData = {
   "ABC123": {
